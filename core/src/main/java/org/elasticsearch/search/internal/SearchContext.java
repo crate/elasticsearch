f4a1a2675c9a0ae78502460809ab9927e48eb672
Sebastian Utz <su@rtme.net>
2016-04-19 14:22:56 +0200

    makes SearchContext.close() non-final
    This gives Crate the possibility to simple mock
    SearchContext child implementations and spy calls
    to close()
    E.g. the JobCollectContextTest class relies on that.

--- core/src/main/java/org/elasticsearch/search/internal/SearchContext.java
+++ core/src/main/java/org/elasticsearch/search/internal/SearchContext.java
@@ -118,7 +118,7 @@ public abstract class SearchContext extends DelegatingHasContextAndHeaders imple
     }
 
     @Override
-    public final void close() {
+    public void close() {
         if (closed.compareAndSet(false, true)) { // prevent double closing
             decRef();
         }
